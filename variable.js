e2d0dcfe8b3a8a259e4406fb12da0a0d82164bb9
Laxman Lingampally <laxmanl@virtusa.com>
2020-07-11 12:25:01 +0530

    javascript basics

---
+++ variable.js
@@ -0,0 +1,16 @@
+var fullName = "Laxman L"; 
+
+//How to name variables
+//fullName
+//full-name
+//full_name
+
+var courseName="ReactJS Bootcamp";
+var isLoggedIn = false;
+var loggedCount = 34;
+console.log(loggedCount)
+console.log("loggedCount")
+var paymentMode;
+console.log(paymentMode)
+paymentMode = "Credit card";
+console.log(paymentMode)
---
+++ variables-two.js
@@ -0,0 +1,18 @@
+const uid = "abc123";
+
+var fullName="Laxman L";
+var email = "test@gmail.com";
+var password = "asda";
+var confirmPassword = "asda";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+
+
+//fullName = prompt("Enter your name!");
+
+console.log(fullName);
+console.log(`
+     Full Nmae: ${fullName}
+     User ID: ${uid}
+     and Email: ${email}`);
